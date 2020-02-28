3933e2f601611aa74b8d8ad908c81c8ed72ca0c4
victoe01 <victoria.elliott@nhs.net>
2020-02-28 13:42:04 +0000

    introducing functions

---
+++ 2_Functions.R
@@ -0,0 +1,96 @@
+
+
+###############################################.
+## File Paths ----
+###############################################.
+
+
+data_folder <- "/PHI_conf/ScotPHO/HfA/Data/"
+lookups <- "/PHI_conf/ScotPHO/HfA/Data/Lookups/"
+
+
+###############################################.
+## Functions ----
+###############################################.
+
+
+# Age groups
+#Function to create age groups needed for standardization
+
+create_agegroups <- function(dataset) {
+  dataset %>% mutate(age_grp = as.character(case_when(between(age, 0, 4) ~ 1,
+                                                      between(age, 5, 9) ~ 2, between(age, 10, 14) ~ 3, between(age, 15, 19) ~ 4, 
+                                                      between(age, 20, 24) ~ 5, between(age, 25, 29) ~ 6, between(age, 30, 34) ~ 7, 
+                                                      between(age, 35, 39) ~ 8, between(age, 40, 44) ~ 9, between(age, 45, 49) ~ 10, 
+                                                      between(age, 50, 54) ~ 11, between(age, 55, 59) ~ 12, between(age, 60, 64) ~ 13,
+                                                      between(age, 65, 69) ~ 14, between(age, 70, 74) ~ 15,  between(age, 75, 79) ~ 16,
+                                                      between(age, 80, 84) ~ 17, between(age, 85, 89) ~ 18, between(age, 90, 200) ~ 19)))
+}
+
+
+
+
+# Function to calculate age sex standardized rates
+
+# Parameters:
+# filename : name of indicator being produced
+# pop: specify which population (e.g. allages, 65+, 0to64 to ensure populations match)
+# epop_total: figure to use for total european standard pop
+# ind_id: HFA indicator number
+
+create_rates <- function(filename, pop, epop_total, ind_id) {
+  
+  #read in required population file
+  pop_lookup <-readRDS(paste0(lookups,"Population/scot_pop_",pop,".rds"))
+  
+  #read in indicator data file
+  data_indicator <-readRDS(paste0(data_folder,"Prepared Data/",filename,"_raw.rds"))
+  
+  #full join to add population
+  data_indicator <- full_join(x = data_indicator, y = pop_lookup, by = c("year", "sex_grp", "age_grp"))
+  
+  #add epop (currently no indicators which don't include full 5 year ageband epop)
+  data_indicator$epop <- recode(as.character(data_indicator$age_grp),
+                                "1" = 5000, "2" = 5500, "3" = 5500, "4" = 5500, 
+                                "5" = 6000, "6" = 6000, "7" = 6500, "8" = 7000, 
+                                "9" = 7000, "10" = 7000, "11" =7000, "12" = 6500, 
+                                "13" = 6000, "14" = 5500, "15" = 5000,
+                                "16" = 4000, "17" = 2500, "18" = 1500, "19" = 1000)
+  
+  
+  # Calculating individual easr and variance
+  data_indicator %<>%
+    mutate(easr_first = numerator*epop/denominator) %>% # easr population
+    # Converting Infinites to NA and NA's to 0s to allow proper functioning
+    na_if(Inf) %>% # Caused by a denominator of 0 in an age group with numerator >0
+    mutate_at(c("easr_first"), ~replace(., is.na(.), 0))
+  
+  # aggregating by sex_grp
+  data_indicator %<>%
+    subset(select = -c(age_grp)) %>% #remove age groups to calculate of all ages by year andsex_grp
+    group_by(year, sex_grp) %>% summarise_all(sum, na.rm =T) %>% ungroup() 
+  
+  data_indicator %<>%
+    mutate(epop_total=epop_total,
+           easr = easr_first/epop_total, # easr calculation
+           rate = easr * 100000) %>%  # rate calculation
+    select(-c(easr_first, epop_total, easr, epop))
+ 
+# Preparing data for Shiny tool
+  data_shiny <- data_indicator %>%
+    mutate(ind_id=ind_id,
+           country="Scotland") %>%
+    rename(value=rate, sex=sex_grp) %>%
+    select(c(year, value, country, sex, ind_id))
+
+# Making final dataset available outside the function
+final_result <<- data_shiny
+
+# Save RDS file for checking
+saveRDS(data_shiny, file = paste0(data_folder, "Data to be checked/",ind_id,"_",filename, ".rds"))
+
+
+}
+
+
+
--- indicator_production_ld.R
+++ indicator_production_ld.R
@@ -3,6 +3,10 @@
 ## Diseases of the circulatory system, all ages, per 100 000, by sex (age-standardized death rate)
 
 ## ICD10 CODES: I00-I99
+## For cross referencing purposes comparable figures published by British Heart Foundation for all constituent UK nations
+## https://www.bhf.org.uk/what-we-do/our-research/heart-statistics/heart-statistics-publications/cardiovascular-disease-statistics-2019
+## BHF figures are not as up to date as figures ISD are able to produce.
+
 
 ###############################################.
 ## Packages/Filepaths ----
@@ -14,75 +18,23 @@ library(readr) # writing csv's
 library(magrittr) # for other pipe operators
 
 
-pop_lookup <- "/PHI_conf/ScotPHO/HfA/Data/Lookups/Pops/"
+#pop_lookup <- "/PHI_conf/ScotPHO/HfA/Data/Lookups/Pops/"
+
+source("2_Functions.R") #Normal indicator functions
 
 
 
 ###############################################.
-## Functions ----
-###############################################.
-
-# creating age groups for EASR
-create_agegroups <- function(dataset) {
-  dataset %>% mutate(age_grp = case_when(between(age, 0, 4) ~ 1,
-                                         between(age, 5, 9) ~ 2, between(age, 10, 14) ~ 3, 
-                                         between(age, 15, 19) ~ 4, between(age, 20, 24) ~ 5, 
-                                         between(age, 25, 29) ~ 6, between(age, 30, 34) ~ 7, 
-                                         between(age, 35, 39) ~ 8, between(age, 40, 44) ~ 9, 
-                                         between(age, 45, 49) ~ 10, between(age, 50, 54) ~ 11, 
-                                         between(age, 55, 59) ~ 12, between(age, 60, 64) ~ 13,
-                                         between(age, 65, 69) ~ 14, between(age, 70, 74) ~ 15,  
-                                         between(age, 75, 79) ~ 16, between(age, 80, 84) ~ 17, 
-                                         between(age, 85, 89) ~ 18, between(age, 90, 200) ~ 19))
-}
-
-# Add European Population for rate calculation
-add_epop <- function(dataset) {
-  dataset <- dataset %>% 
-    mutate(epop = recode(as.character(age_grp), # EASR age group pops
-                         "1"=5000, "2"=5500, "3"=5500, "4"=5500, "5"=6000, 
-                         "6"=6000, "7"= 6500, "8"=7000, "9"=7000, "10"=7000,
-                         "11"=7000, "12"=6500, "13"=6000, "14"=5500, "15"=5000,
-                         "16"= 4000, "17"=2500, "18"=1500, "19"=1000)) 
-}
-
-# Function to calculate age sex standardized rates
-create_rates <- function(dataset, epop_total, sex_grp, cats = NULL ) {
-  dataset <- dataset %>%
-    mutate(easr_first = numerator*epop/denominator) # easr population
-  
-  if (sex_grp == T) {
-    # aggregating by year, code and time
-    dataset <- dataset %>% select(-age_grp) %>%
-      group_by_at(c(cats, "year", "sex_grp")) %>% 
-      summarize_at(c("numerator", "easr_first"), sum, na.rm = T) %>% ungroup()    
-  } else if (sex_grp == F) {
-    # aggregating by year, code and time
-    dataset <- dataset %>% select(-age_grp, -sex_grp) %>%
-      group_by_at(c(cats, "year")) %>% 
-      summarize_at(c("numerator", "easr_first"), sum, na.rm = T) %>% ungroup()
-  }
-  
-  # Calculating rates
-  dataset <- dataset %>%
-    mutate(epop_total = epop_total,  # Total EPOP population
-           easr = easr_first/epop_total, # easr calculation
-           rate = easr*100000) %>%   # rate calculation
-    select(-c(easr_first, epop_total, easr))
-}
-
-###############################################.
-## Extract data from SMRA ----
+## Part 1 - Extract data from SMRA ----
 ###############################################.
 # SMRA login information
 channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
                                       uid=.rs.askForPassword("SMRA Username:"), 
                                       pwd=.rs.askForPassword("SMRA Password:")))
 
-
 # SQL query for drug deaths 2006-2018
 # Deaths for scottish residents are coded as (XS)
-circulatory_system_deaths <- tbl_df(dbGetQuery(channel, statement=
+circulatory_deaths_extract <- tbl_df(dbGetQuery(channel, statement=
                                               "SELECT year_of_registration year, age, SEX sex_grp, UNDERLYING_CAUSE_OF_DEATH cod1, POSTCODE pc7
                                                FROM ANALYSIS.GRO_DEATHS_C 
                                                WHERE date_of_registration between '1 January 2000' and '31 December 2018'
@@ -95,62 +47,54 @@ circulatory_system_deaths <- tbl_df(dbGetQuery(channel, statement=
   create_agegroups() %>% 
   mutate(sex_grp = recode(sex_grp, "1" = "Male", "2" = "Female"))
 
-# deaths dataframe by gender 
-circulatory_system_deaths_sex <- circulatory_system_deaths %>%
+# deaths by gender 
+circulatory_deaths_sex <- circulatory_deaths_extract %>%
   group_by(year, age_grp, sex_grp) %>% count() %>% #aggregating
   ungroup()
 
-# deaths dataframe all
-circulatory_system_deaths_all <- circulatory_system_deaths_sex %>%
+# deaths for all
+circulatory_deaths_all <- circulatory_deaths_sex %>%
   group_by(year, age_grp) %>%
   summarise(n =sum(n)) %>% mutate(sex_grp = "All") %>%
   ungroup()
 
-#combined dataframe
-circulatory_system_deaths_total <- rbind(circulatory_system_deaths_sex, 
-                                         circulatory_system_deaths_all)
-
-
-
-## Section below could ultimately become part of function
-
-#parameters
-data_indicator <- circulatory_system_deaths_total %>%
+#combine datasets
+circulatory_deaths <- rbind(circulatory_deaths_sex,circulatory_deaths_all) %>%
   rename(numerator = n)
 
-epop_total <-c(200000,100000)
+
+###############################################.
+## Part 2 - Create the different geographies basefiles ----
+###############################################.
+
+
+# Circulatory deaths - All Ages
+saveRDS(circulatory_deaths, file=paste0(data_folder, 'Prepared Data/circulatory deaths_allages_raw.rds'))
+
+# Circulatory deaths - Ages 0 to 64 years
+circulatory_deaths_0to64 <-circulatory_deaths %>% subset(as.numeric(age_grp)<=13)
+saveRDS(circulatory_deaths, file=paste0(data_folder, 'Prepared Data/circulatory deaths_0to64_raw.rds'))
+
+# Circulatory deaths - Ages 65+ years
+circulatory_deaths_0to64 <-circulatory_deaths %>% subset(as.numeric(age_grp)>=14)
+saveRDS(circulatory_deaths, file=paste0(data_folder, 'Prepared Data/circulatory deaths_65andover_raw.rds'))
 
 
 
-##read in population file
-pop_lookup <-readRDS("/PHI_conf/ScotPHO/HfA/Data/Lookups/Pops/scot_pop_allages.rds")
+###############################################.
+## Part 3 - Call function to calculate rates ----
+###############################################.
 
 
-##full join to add population
-data_indicator <- full_join(x = data_indicator, y = pop_lookup, 
-                            by = c("year", "sex_grp", "age_grp"))
+# Circulatory deaths - all ages
+create_rates(filename = "circulatory deaths_allages", pop="allages", epop_total = 100000, ind_id="HFA_101")
 
-data_indicator <- data_indicator %>% add_epop() # EASR age group pop
+# Circulatory deaths - 0-64years
+create_rates(filename = "circulatory deaths_0to64", pop="0to64", epop_total = 79000, ind_id="HFA_98")
 
-dataset <- data_indicator %>%
-  mutate(easr_first = numerator*epop/denominator) %>% # easr population
-  # Converting Infinites to NA and NA's to 0s to allow proper functioning
-  na_if(Inf) %>% # Caused by a denominator of 0 in an age group with numerator >0
-  mutate_at(c("easr_first"), ~replace(., is.na(.), 0))
-
-
-# aggregating by year, code and time
-dataset %<>% subset(select = -c(age_grp)) %>%
-  group_by(year, sex_grp) %>% summarise_all(sum, na.rm =T) %>% ungroup() %>%
-  mutate(epop_total=case_when(sex_grp=="All" ~ epop_total[1], TRUE~epop_total[2]),
-         easr = easr_first/epop_total, # easr calculation
-         rate = easr * 100000) %>%  # rate calculation
- select(-c(easr_first, epop_total, easr, epop))
+# Circulatory deaths - 65+years
+create_rates(filename = "circulatory deaths_65andover", pop="65+", epop_total = 21000, ind_id="HFA_104")
 
 
 
 
-
-easr_circ_system_deaths <- create_rates(dataset = test_deaths, epop_total = 100000, sex_grp = T)
-
-easr_circ_system_deaths2 <- create_rates(dataset = test_deaths, epop_total = 200000, sex_grp = F)
