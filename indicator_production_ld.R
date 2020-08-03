b393237b665dd87506f93a479250c66e6223c22f
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-08-03 12:58:04 +0100

    tidy up, start including more indicators

--- indicator_production_ld.R
+++ indicator_production_ld.R
@@ -7,57 +7,39 @@
 ## https://www.bhf.org.uk/what-we-do/our-research/heart-statistics/heart-statistics-publications/cardiovascular-disease-statistics-2019
 ## BHF figures are not as up to date as figures ISD are able to produce.
 
-
 ###############################################.
 ## Packages/Filepaths ----
 ###############################################.
-
-library(dplyr) # for data manipulation
-library(tidyr) # for data manipulation
-library(readr) # writing csv's
-library(magrittr) # for other pipe operators
-library(odbc)
-
-
-#pop_lookup <- "/PHI_conf/ScotPHO/HfA/Data/Lookups/Pops/"
-
 source("2_Functions.R") #Normal indicator functions
 
-
-
 ###############################################.
-## Part 1 - Extract data from SMRA ----
+## Part 1 - Extract data from SMRA and prepare basefiles ----
 ###############################################.
 # SMRA login information
 channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
                                       uid=.rs.askForPassword("SMRA Username:"), 
                                       pwd=.rs.askForPassword("SMRA Password:")))
 
-extract_deaths(diag = "^I[00-99]", filename = "circulatory", age064 = F, plus65 = F)
-
-
-###############################################.
-## Part 2 - Create the different geographies basefiles ----
-###############################################.
-
-
-
-
+# Circulatory deaths
+extract_deaths(diag = "^I[00-99]", filename = "circulatory",  age064 = TRUE, plus65 = TRUE)
+# Ischaemic heart disease deaths
+extract_deaths(diag = "^I2[0-5]", filename = "ischaemic", age064 = TRUE, plus65 = TRUE)
+# Cerebrovascular disease deaths
+extract_deaths(diag = "^I6", filename = "cerebrovascular", age064 = TRUE, plus65 = TRUE)
 
 ###############################################.
-## Part 3 - Call function to calculate rates ----
+## Part 2 - Call function to calculate rates ----
 ###############################################.
-
-
-# Circulatory deaths - all ages
-create_rates(filename = "circulatory deaths_allages", pop="allages", epop_total = 100000, ind_id="HFA_101")
-
-# Circulatory deaths - 0-64years
-create_rates(filename = "circulatory deaths_0to64", pop="0to64", epop_total = 79000, ind_id="HFA_98")
-
-# Circulatory deaths - 65+years
-create_rates(filename = "circulatory deaths_65andover", pop="65+", epop_total = 21000, ind_id="HFA_104")
-
-
-
+# Circulatory deaths 
+create_rates(filename = "circulatory_deaths_allages", pop="allages", epop_total = 100000, ind_id="HFA_101")
+create_rates(filename = "circulatory_deaths_0to64", pop="0to64", epop_total = 79000, ind_id="HFA_98")
+create_rates(filename = "circulatory_deaths_65andover", pop="65+", epop_total = 21000, ind_id="HFA_104")
+# Ischaemic heart disease deaths 
+create_rates(filename = "ischaemic_deaths_allages", pop="allages", epop_total = 100000, ind_id="HFA_110")
+create_rates(filename = "ischaemic_deaths_0to64", pop="0to64", epop_total = 79000, ind_id="HFA_107")
+create_rates(filename = "ischaemic_deaths_65andover", pop="65+", epop_total = 21000, ind_id="HFA_113")
+# Cerebrovascular disease deaths 
+create_rates(filename = "cerebrovascular_deaths_allages", pop="allages", epop_total = 100000, ind_id="HFA_119")
+create_rates(filename = "cerebrovascular_deaths_0to64", pop="0to64", epop_total = 79000, ind_id="HFA_116")
+create_rates(filename = "cerebrovascular_deaths_65andover", pop="65+", epop_total = 21000, ind_id="HFA_122")
 
